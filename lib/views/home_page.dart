4685a651448400d183ab2e462d98a00fd8e39b2f
NikelMaharjan <nikelmaharjan@gmail.com>
2022-12-30 14:19:50 +0545

    changes

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -61,156 +61,17 @@ class HomePage extends StatelessWidget {
             SingleChildScrollView(
               physics: const BouncingScrollPhysics(),
               scrollDirection: Axis.horizontal,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-
-                  SizedBox(width: 12,),
-
-                  Stack(
-                    children: [
-
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.green[300],
-                        ),
-
-                        height: 220,
-                        width: 120,
-                      ),
-                      const Positioned(
-                        left: 10,
-                        top: 10,
-                        child: CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.blue,
-                          child: CircleAvatar(
-                            radius: 18,
-                            backgroundColor: grey,
-                          ),
-                        ),
-                      ),
-
-                      const Positioned(
-                          bottom: 0,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text("Nikel Maharajan",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),),
-                          )),
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
 
 
-                    ],
-                  ),
-                  Stack(
-                    children: [
-
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.green[300],
-                        ),
-
-                        height: 220,
-                        width: 120,
-                      ),
-                      const Positioned(
-                        left: 10,
-                        top: 10,
-                        child: CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.blue,
-                          child: CircleAvatar(
-                            radius: 18,
-                            backgroundColor: grey,
-                          ),
-                        ),
-                      ),
-
-                      const Positioned(
-                          bottom: 0,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text("Nikel Maharajan",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),),
-                          )),
-
-
-                    ],
-                  ),
-                  Stack(
-                    children: [
-
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.green[300],
-                        ),
-
-                        height: 220,
-                        width: 120,
-                      ),
-                      const Positioned(
-                        left: 10,
-                        top: 10,
-                        child: CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.blue,
-                          child: CircleAvatar(
-                            radius: 18,
-                            backgroundColor: grey,
-                          ),
-                        ),
-                      ),
-
-                      const Positioned(
-                          bottom: 0,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text("Nikel Maharajan",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),),
-                          )),
-
-
-                    ],
-                  ),
-                  Stack(
-                    children: [
-
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.green[300],
-                        ),
-
-                        height: 220,
-                        width: 120,
-                      ),
-                      const Positioned(
-                        left: 10,
-                        top: 10,
-                        child: CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.blue,
-                          child: CircleAvatar(
-                            radius: 18,
-                            backgroundColor: grey,
-                          ),
-                        ),
-                      ),
-
-                      const Positioned(
-                          bottom: 0,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text("Nikel Maharajan",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),),
-                          )),
-
-
-                    ],
-                  ),
+                    _buildStories(),
+                    _buildStories(),
+                    _buildStories(),
+                    _buildStories(),
 
 
 
@@ -221,7 +82,8 @@ class HomePage extends StatelessWidget {
 
 
 
-                ],
+                  ],
+                ),
               ),
             ),
 
@@ -233,170 +95,13 @@ class HomePage extends StatelessWidget {
 
 
 
-            Row(
-              children: [
-
-                Container(
-                  margin: EdgeInsets.only(right: 10, left: 10),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 22,
-                  ),
-                ),
-
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                       Text("Doctor who", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-                      Row(
-                        children: const [
-                          Text("20h", style: TextStyle(fontSize: 12),),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Icon(
-                            Icons.timelapse,
-                            size: 12,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                const Icon(Icons.more_horiz_outlined),
-                SizedBox(width: 10,),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 4),
-                    child: const Icon(Icons.close)),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 260,
-              color: Colors.grey,
-            ),
-
-            SizedBox(height: 10,),
+            _buildNewsFeeds(),
+            _buildNewsFeeds(),
 
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.thumb_up_alt_rounded, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Like"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.comment, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Comment"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.share, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Share",),
-                  ],
-                ),
-
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              height: 10,
-              color: const Color(0xffCACCD2),
-            ),
 
 
-            Row(
-              children: [
 
-                Container(
-                  margin: EdgeInsets.only(right: 10, left: 10),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 22,
-                  ),
-                ),
-
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Doctor who", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-                      Row(
-                        children: const [
-                          Text("20h", style: TextStyle(fontSize: 12),),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Icon(
-                            Icons.timelapse,
-                            size: 12,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                 Icon(Icons.more_horiz_outlined),
-                 Container(
-                   margin: EdgeInsets.symmetric(horizontal: 4),
-                     child: Icon(Icons.close)),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 260,
-              color: Colors.grey,
-            ),
-
-            SizedBox(height: 10,),
-
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.thumb_up_alt_rounded, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Like"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.comment, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Comment"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.share, color: Colors.grey,),
-                    SizedBox(width: 6,),
-                    Text("Share",),
-                  ],
-                ),
-
-              ],
-            ),
-
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              height: 10,
-              color: const Color(0xffCACCD2),
-            ),
 
 
 
@@ -407,5 +112,180 @@ class HomePage extends StatelessWidget {
         ),
       )
     );
+  }
+
+  Column _buildNewsFeeds() {
+    return Column(
+            children: [
+              Row(
+                children: [
+
+                  Container(
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      radius: 22,
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Doctor who", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                        Row(
+                          children: const [
+                            Text("20h", style: TextStyle(fontSize: 12),),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Icon(
+                              Icons.timelapse,
+                              size: 12,
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Icon(Icons.more_horiz_outlined),
+                  SizedBox(width: 10,),
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4),
+                      child: const Icon(Icons.close)),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 260,
+                color: Colors.grey,
+              ),
+
+              SizedBox(height: 10,),
+
+
+              Container(
+
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Row(
+
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              width: 54,
+                            ),
+                            Positioned(
+                                left: 28,
+                                child: Image.network("https://cdn3.iconfinder.com/data/icons/object-emoji/50/Heart-128.png", height: 20, width: 20,)),
+
+                            Positioned(
+                                left: 14,
+                                child: Image.network("https://cdn4.iconfinder.com/data/icons/emoji-66/64/21-haha-512.png", height: 20, width: 20,)),
+                            Image.network("https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/199_Like_logo_logos-512.png", height: 20, width: 20, color: blue,),
+
+                          ],
+                        ),
+                        Text("4.1k", style: TextStyle(color: Colors.grey),)
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Text("290 Comments", style: TextStyle(color: Colors.grey),),
+                        SizedBox(width: 10,),
+                        Text("90 Shares ", style: TextStyle(color: Colors.grey),),
+                      ],
+                    )
+
+
+                  ],
+                ),
+              ),
+
+              Divider(
+                color: Colors.grey,
+              ),
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.thumb_up_alt_rounded, color: Colors.grey,),
+                      SizedBox(width: 6,),
+                      Text("Like"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.comment, color: Colors.grey,),
+                      SizedBox(width: 6,),
+                      Text("Comment"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.share, color: Colors.grey,),
+                      SizedBox(width: 6,),
+                      Text("Share",),
+                    ],
+                  ),
+
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                height: 10,
+                color: const Color(0xffCACCD2),
+              ),
+            ],
+          );
+  }
+
+  Widget _buildStories() {
+    return Stack(
+                  children: [
+
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(20)),
+                        color: Colors.green[300],
+                      ),
+
+                      height: 220,
+                      width: 120,
+                    ),
+                    const Positioned(
+                      left: 10,
+                      top: 10,
+                      child: CircleAvatar(
+                        radius: 22,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          radius: 18,
+                          backgroundColor: grey,
+                        ),
+                      ),
+                    ),
+
+                    const Positioned(
+                        bottom: 0,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Nikel Maharajan",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14),),
+                        )),
+
+
+                  ],
+                );
   }
 }
